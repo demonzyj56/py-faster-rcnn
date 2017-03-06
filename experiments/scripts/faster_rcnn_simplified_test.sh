f9fe8f1c257b4145eb774670fba24599a24909ad
leoyolo <demonzyj56@gmail.com>
2017-03-06 19:37:44 +0800

    Add script for testing only

---
+++ experiments/scripts/faster_rcnn_simplified_test.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Usage:
+# ./experiments/scripts/faster_rcnn_simplified_test.sh GPU NET DATASET [options args to {train,test}_net.py]
+# DATASET is either pascal_voc or coco.
+#
+# Example:
+# ./experiments/scripts/faster_rcnn_simplified_test.sh 0 VGG_CNN_M_1024 pascal_voc \
+#   --set EXP_DIR foobar RNG_SEED 42 TRAIN.SCALES "[400,500,600,700]"
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+GPU_ID=$1
+NET=$2
+NET_lc=${NET,,}
+DATASET=$3
+
+array=( $@ )
+len=${#array[@]}
+EXTRA_ARGS=${array[@]:3:$len}
+EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
+
+case $DATASET in
+  pascal_voc)
+    TRAIN_IMDB="voc_2007_trainval"
+    TEST_IMDB="voc_2007_test"
+    PT_DIR="pascal_voc"
+    ITERS=70000
+    ;;
+  coco)
+    # This is a very long and slow training schedule
+    # You can probably use fewer iterations and reduce the
+    # time to the LR drop (set in the solver to 350,000 iterations).
+    TRAIN_IMDB="coco_2014_train"
+    TEST_IMDB="coco_2014_minival"
+    PT_DIR="coco"
+    ITERS=490000
+    ;;
+  *)
+    echo "No dataset given"
+    exit
+    ;;
+esac
+
+LOG_PATH="experiments/logs/faster_rcnn_simplified"
+mkdir -p ${LOG_PATH}
+# LOG="experiments/logs/faster_rcnn_simplified_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG=${LOG_PATH}/"test_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+# time ./tools/train_net.py --gpu ${GPU_ID} \
+#   --solver models/${PT_DIR}/${NET}/faster_rcnn_simplified/solver.prototxt \
+#   --weights data/imagenet_models/${NET}.v2.caffemodel \
+#   --imdb ${TRAIN_IMDB} \
+#   --iters ${ITERS} \
+#   --cfg experiments/cfgs/faster_rcnn_simplified.yml \
+#   ${EXTRA_ARGS}
+
+# set +x
+# NET_FINAL=`grep -B 1 "done solving" ${LOG} | grep "Wrote snapshot" | awk '{print $4}'`
+# set -x
+NET_FINAL=output/faster_rcnn_simplified/voc_2007_trainval/vgg16_faster_rcnn_simplified_iter_70000.caffemodel
+
+time ./tools/test_net.py --gpu ${GPU_ID} \
+  --def models/${PT_DIR}/${NET}/faster_rcnn_simplified/test.prototxt \
+  --net ${NET_FINAL} \
+  --imdb ${TEST_IMDB} \
+  --cfg experiments/cfgs/faster_rcnn_simplified.yml \
+  --vis \
+  ${EXTRA_ARGS}

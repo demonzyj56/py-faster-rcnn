7023dbb1bd099d016a1a67de8e8987fabd61c68d
leoyolo <demonzyj56@gmail.com>
2017-03-14 16:52:35 +0800

    Add scripts for training/testing under super resoltuion settings

--- experiments/scripts/fast_rcnn_sr.sh
+++ experiments/scripts/fast_rcnn_sr.sh
@@ -27,7 +27,7 @@ case $DATASET in
     TRAIN_IMDB="voc_2007_trainval"
     TEST_IMDB="voc_2007_test"
     PT_DIR="pascal_voc"
-    ITERS=40000
+    ITERS=60000
     ;;
   coco)
     TRAIN_IMDB="coco_2014_train"

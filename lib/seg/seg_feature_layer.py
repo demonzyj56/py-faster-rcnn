9220452b85b5f531aaa4d22c95561cf9ea6faf78
leoyolo <demonzyj56@gmail.com>
2017-03-24 13:19:51 +0800

    Use class prob only

--- lib/seg/seg_feature_layer.py
+++ lib/seg/seg_feature_layer.py
@@ -65,7 +65,7 @@ class SegFeatureLayer(caffe.Layer):
         if cfg.DEBUG:
             from ipdb import set_trace; set_trace()
         self.cfg_key = 'TRAIN' if self.phase == 0 else 'TEST'
-        _gpu_id = self.layer_params.get('gpu_id', 0)
+        #  _gpu_id = self.layer_params.get('gpu_id', 0)
         _this_dir = osp.dirname(__file__)
         _net_def = osp.join(_this_dir, 'pascalcontext-fcn8s-deploy.prototxt')
         _net_weight = osp.join(cfg.DATA_DIR, 'pascalcontext-fcn8s',
@@ -102,8 +102,13 @@ class SegFeatureLayer(caffe.Layer):
         self.seg_net.blobs['rois'].reshape(*bottom[1].data.shape)
         self.seg_net.blobs['rois'].data[...] = bottom[1].data
         seg_features = self.seg_net.forward()
-        top[0].reshape(*seg_features['seg_features'].shape)
-        top[0].data[...] = seg_features['seg_features']
+        seg_cls = seg_features['seg_features']
+        seg_cls[:, 0] += np.sum(seg_cls[:, 21:], axis=1)
+        seg_cls = seg_cls[:, :21]
+        top[0].reshape(*seg_cls.shape)
+        top[0].data[...] = seg_cls
+        #  top[0].reshape(*seg_features['seg_features'].shape)
+        #  top[0].data[...] = seg_features['seg_features']
 
     def backward(self, top, propagate_down, bottom):
         pass
